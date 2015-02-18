8affe3d8fb5a6d60d907c3acc7e48900774d458a
Jack Kettle <jack.kettle2@mail.dcu.ie>
2015-02-18 17:29:01 +0000

    Added some header styling and structre

--- js/dev/main.js
+++ js/dev/main.js
@@ -12,10 +12,40 @@ app.config(function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: "/",
       templateUrl: "partials/home.html",
-      controller: function () {
-        console.log("Controller");
-      }
+      data: {
+        linkText: "Home"
+      } 
     })
     
-    // Add new pages here
+    .state('fileSaverTest', {
+      url: "/fileSaverTest",
+      templateUrl: "partials/fileSaverTest.html",
+      data: {
+        linkText: "FileSaver"
+      } 
+    })
 })
+
+app.controller('mainNavController', function ($scope, $state) {
+  
+  // Set main navigation from existing router-ui states
+  $scope.navigationData = [];
+  var data = $state.get()
+  angular.forEach(data, function(index) {
+    if(index.name !== ""){
+      $scope.navigationData.push(index);
+    }
+  });
+  
+})
+
+app.directive('mainNav', function () {
+  return {
+    restrict: 'A',
+    replace: 'true',
+    template:
+      '<li ng-repeat="navObject in navigationData">' +
+      '<a role="presentation" ui-sref="{{ navObject.name }}">{{ navObject.data.linkText }}</a>' +
+      '</li>'
+  }
+});
